b11e585d1c69da95c1c3002615460de47f852345
christopherpintoc <146459577+christopherpintoc@users.noreply.github.com>
2024-01-31 18:05:00 -0300

    Delete src directory

--- src/app/services/paginatorIntl.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable}  from "@angular/core";
-import { MatPaginatorIntl} from "@angular/material/paginator";
-
-@Injectable()
-export class PaginatorIntl extends MatPaginatorIntl {
-  override getRangeLabel = (page: number, pageSize: number, length: number) => {
-    return `Page ${page + 1} of ${Math.ceil(length/pageSize)}`
-  }
-}
